6d8a75e0f4aed50e3a85f3e6f494e5825296bab2
edwardliqi <48934081+edwardliqi@users.noreply.github.com>
2021-05-19 16:23:58 +0800

    bug fix

--- server.py
+++ server.py
@@ -19,11 +19,11 @@ host_dict = dict()
 def process_item(telegram):
     tele = Telegram.decoding(telegram)
     lock.acquire()
-    #if host_dict.__contains__(tele._hostname):
-    #    tree.item(host_dict[tele._hostname], values= [tele._hostname, tele._cpu_usage] + [tele[k]._freespace for k in cols[2: ]])
-    #else:
-    this_item = tree.insert('', 'end',text=tele._hostname, values=[tele._hostname, tele._cpu_usage] + [tele[k]._freespace for k in cols[2: ]])
-    host_dict[tele._hostname] = this_item
+    if host_dict.__contains__(tele._hostname):
+        tree.item(host_dict[tele._hostname], values= [tele._hostname, tele._cpu_usage] + [tele[k]._freespace for k in cols[2: ]])
+    else:
+        this_item = tree.insert('', 'end',text=tele._hostname, values=[tele._hostname, tele._cpu_usage] + [tele[k]._freespace for k in cols[2: ]])
+        host_dict[tele._hostname] = this_item
     lock.release()
 
 def treeview_sort_column(tv, col, reverse):
